da490d34d5234f0a116af8552cc2fe0e7214c6e0
minseong0213 <minseong0213@naver.com>
2023-11-27 20:11:31 +0900

    resolve merge conflicts

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const test = require("./Router/test");
+const mysql = require("mysql");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "alstjd48119!",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("connect");
+});
 
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
