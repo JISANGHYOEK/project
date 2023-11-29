2986201cd70c53fa8b53c8c7c6e6111a68277b68
Minsu Yoo <minsoo0515@naver.com>
2023-11-30 01:25:00 +0900

    feat.post

--- Server/db.js
+++ Server/db.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
+const mysql = require("mysql");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 
 const options = {
-   host: '10000mr.com',
-   port: 3306,
-   user: 'root',
-   password: 'Hyeok0901!',
-   database: 'manlab_db',
+  host: "10000mr.com",
+  port: 3306,
+  user: "root",
+  password: "Hyeok0901!",
+  database: "manlab_db",
 };
 
 const sessionStore = new MySQLStore(options);
@@ -17,29 +17,30 @@ const sessionStore = new MySQLStore(options);
 const con = mysql.createConnection(options);
 
 con.connect(function (err) {
-   if (err) throw err;
-   console.log('connect');
+  if (err) throw err;
+  console.log("connect");
 });
 
 app.use(
-   session({
-      key: 'session_cookie_name',
-      secret: 'session_cookie_secret',
-      store: sessionStore,
-      resave: false,
-      saveUninitialized: false,
-   }),
+  session({
+    key: "session_cookie_name",
+    secret: "session_cookie_secret",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 },
+  })
 );
 
 sessionStore
-   .onReady()
-   .then(() => {
-      // MySQL session store ready for use.
-      console.log('MySQLStore ready');
-   })
-   .catch(error => {
-      // Something went wrong.
-      console.error(error);
-   });
+  .onReady()
+  .then(() => {
+    // MySQL session store ready for use.
+    console.log("MySQLStore ready");
+  })
+  .catch((error) => {
+    // Something went wrong.
+    console.error(error);
+  });
 
 module.exports = { con, app };
