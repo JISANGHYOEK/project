3c10ef53cb000da41a173a7ffa6b67222196ab47
0verfl0w767 <tkddbs7631@naver.com>
2023-12-02 13:40:52 +0900

    fix: chagned login & logout sessionStorage

--- web/src/store.js
+++ web/src/store.js
@@ -1,29 +1,38 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware';
 import axios from "axios";
 
-export const useStore = create((set) => ({
-  isLogin: false,
-  logIn: async (data) => {
-    try {
-      const response = await axios.post("/api/login", data, { withCredentials: true });
-      if (response.status === 200) {
-        set({ isLogin: true });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  },
-  logOut: async () => {
-    try {
-      const response = await axios.post("/api/logout", { withCredentials: true });
-      if (response.status === 200) {
-        set({ isLogin: false });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  },
-}));
+export const useStore = create(
+  persist(
+    (set, get) => ({
+      isLogin: false,
+      logIn: async (data) => {
+        try {
+          const response = await axios.post("/api/login", data, { withCredentials: true });
+          if (response.status === 200) {
+            set({ isLogin: true });
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      logOut: async () => {
+        try {
+          const response = await axios.post("/api/logout", { withCredentials: true });
+          if (response.status === 200) {
+            set({ isLogin: false });
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    }),
+    {
+      name: 'login-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+)
 
 export const adminStore = create((set) => ({
   isAdmin: false,
