8b482c13ab3ca88f881ea284f35d74ebcaa4db58
0verfl0w767 <tkddbs7631@naver.com>
2023-12-04 01:56:29 +0900

    update: checked server <-> local file

--- web/src/components/FreeRe/FreeReWritePageitem.js
+++ web/src/components/FreeRe/FreeReWritePageitem.js
@@ -1,47 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./FreeReWrite.css";
 
 function FreeReWritePageitem() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [stationId, setStationId] = useState("ST-4");
+  // const [image_path, setImage_path] = useState("");
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://10000mr.com:8080/FreeRePage/FreeReWrtiePage")
-  //     .then((response) => {
-  //       setCategory(response.data);
-  //     });
-  // }, []);
+  const navigate = useNavigate();
 
-  // const categories = Object.values(category).map((item) => (
-  //   <option key={item.id} value={item.id}>
-  //     {item.displayName}
-  //   </option>
-  // ));
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+  // const handImage_pathChange = (e) => {
+  //   setImage_path(e.target.value);
+  // };
 
-  const HandleQuestionSubmit = async () => {
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer cognito 의 access token",
+  const handleSubmit = (e) => {
+    // 폼이 제출되었을 때 할 작업들을 처리하는 함수
+    // 여기에 제출된 데이터를 어딘가에 저장하거나 다른 동작을 수행할 수 있습니다.
+    e.preventDefault();
+    if (!title) {
+      alert("제목을 입력해주세요.");
+      return;
+    }
+    if (!content) {
+      alert("내용을 입력해주세요.");
+      return;
+    }
+
+    const data = {
+      title: title,
+      content: content,
+      // image_path: image_path,
     };
 
-    try {
-      const response = await axios.post(
-        "http://10000mr.com:8080/FreeRePage/FreeReWritePage",
-        {
-          title: title,
-          content: content,
-          stationId: stationId,
-        },
-        { headers: headers }
-      );
-
-      console.log("status: " + response.status);
-    } catch (error) {
-      console.log("error: " + error);
-    }
+    axios
+      .post("http://10000mr.com:8080/api/FreeRe", data, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response);
+        navigate("/FreeRePage");
+        alert("게시물이 게시되었습니다.");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("에러가 발생하였습니다.");
+      });
   };
 
   return (
@@ -50,19 +60,24 @@ function FreeReWritePageitem() {
       <div className="FreeRe-view-wrapper">
         <div className="FreeRe-view-row">
           <label>제목</label>
-          <input onChange={(event) => setTitle(event.target.value)}></input>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleTitleChange}
+          />
         </div>
         <div className="FreeRe-view-row">
           <label>내용</label>
           <textarea
-            style={{ height: "500px" }}
-            onChange={(event) => setContent(event.target.value)}
-          ></textarea>
+            name="content"
+            value={content}
+            rows="4"
+            cols="50"
+            onChange={handleContentChange}
+          />
         </div>
-        <button
-          className="FreeRe-view-go-list-btn"
-          onClick={HandleQuestionSubmit}
-        >
+        <button className="FreeRe-view-go-list-btn" onClick={handleSubmit}>
           등록
         </button>
       </div>
